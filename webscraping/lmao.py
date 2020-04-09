1a0ad72141320c5b30e99ab8bf8ce18cb8eeb589
SahilShah518 <sahilshah379@gmail.com>
2020-04-09 19:47:35 -0400

    a

--- webscraping/lmao.py
+++ webscraping/lmao.py
@@ -1,20 +1,64 @@
 import requests
 import pprint
+from geopy.geocoders import Nominatim
 
 pp = pprint.PrettyPrinter(indent=4)
 
-url = 'https://datawrapper.dwcdn.net/H7PJn/8/'
-text = requests.get(url).text
-raw = (text.split('chartData: "')[1]).split('",')[0]
+def main():
+	data = database()
 
-data = []
-rows = raw.split('\\n')
-header = rows[0].split('\\t')
-for r in range(1,len(rows)):
-	column = rows[r].split('\\t')
-	site = {}
-	for c in range(0,len(column)):
-		site[header[c]] = column[c]
-	data.append(site)
+def database():
+	url = 'https://datawrapper.dwcdn.net/H7PJn/8/'
+	text = requests.get(url).text
+	raw = (text.split('chartData: "')[1]).split('",')[0]
+	raw = (raw.replace('\\n\\"','')).replace('\\"','') #bc fuck this
 
-pp.pprint(data)
+	data = []
+	rows = raw.split('\\n')
+	header = rows[0].split('\\t')
+
+	bad = 0
+	for r in range(1,len(rows)):
+		column = rows[r].split('\\t')
+		site = {}
+		for c in range(0,len(header)):
+			site[header[c]] = column[c].strip()
+
+		loc = ''
+		if ('location' not in rows[r]):
+			if site['Address'] != '' and ' ' in site['Address']:
+				loc = site['Address']
+				if site['City'] != '':
+					loc += ' '+site['City']
+				loc += ' '+site['State']
+				loc = loc.replace(' ','+')
+			elif site['Location'] != '':
+				loc = site['Location']
+				if site['City'] != '':
+					loc += ' '+site['City']
+				loc += ' '+site['State']
+				loc = loc.replace(' ','+')
+
+		if loc != '':
+			try:
+				lat,lon = location(loc)
+				site['Latitude'] = lat
+				site['Longitue'] = lon
+
+				print('(%s,%s)' %(lat,lon))
+				data.append(site)
+			except:
+				bad += 1
+		else:
+			bad += 1
+	
+	print(bad)
+	return data
+
+def location(loc):
+	loc_url = 'https://www.google.com/maps/search/'+loc
+	loc = requests.get(loc_url).text
+	return ((loc.split('center=')[1]).split('&amp')[0]).split('%2C')
+
+if __name__ == '__main__':
+	main()
